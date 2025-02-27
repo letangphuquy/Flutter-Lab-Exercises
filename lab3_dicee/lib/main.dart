911b693248d8fa7e64484a8e5112a6e67d9498c2
Le Tang Phu Quy <letangphuquy4@gmail.com>
2025-02-27 15:19:51 +0700

    Lab 3: Bonus. Add RotationTrasition effect

--- lab3_dicee/lib/main.dart
+++ lab3_dicee/lib/main.dart
@@ -13,17 +13,39 @@ class MyApp extends StatefulWidget {
   State<StatefulWidget> createState() => _MyApp();
 }
 
-class _MyApp extends State<MyApp> {
-  int leftDice = 0; // Trying to change state in a StatelessWidget
-  int rightDice = 0;
+class _MyApp extends State<MyApp> with SingleTickerProviderStateMixin {
+  int leftDice = 1; // Trying to change state in a StatelessWidget
+  int rightDice = 1;
+  late AnimationController _controller;
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.elasticOut,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000)
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   void rollLeftDice() {
+    _controller.forward(from: 0.0);
     setState(() {
       leftDice = Random().nextInt(6) % 6 + 1;
     });
   }
 
   void rollRightDice() {
+    _controller.forward(from: 0.0);
     setState(() {
       rightDice = Random().nextInt(6) % 6 + 1;
     });
@@ -51,10 +73,13 @@ class _MyApp extends State<MyApp> {
                         onPressed: rollLeftDice,
                         child: Padding(
                           padding: EdgeInsets.all(4),
-                          child: Image.asset(
-                            "images/dice$leftDice.png",
-                            width: 100,
-                            height: 100,
+                          child: RotationTransition(
+                            turns: _animation,
+                            child: Image.asset(
+                              "images/dice$leftDice.png",
+                              width: 100,
+                              height: 100,
+                            ),
                           ),
                         )),
                   ),
@@ -63,10 +88,13 @@ class _MyApp extends State<MyApp> {
                         onPressed: rollRightDice,
                         child: Padding(
                           padding: EdgeInsets.all(4),
-                          child: Image.asset(
-                            "images/dice$rightDice.png",
-                            width: 100,
-                            height: 100,
+                          child: RotationTransition(
+                            turns: _animation,
+                            child: Image.asset(
+                              "images/dice$rightDice.png",
+                              width: 100,
+                              height: 100,
+                            ),
                           ),
                         )),
                   ),
