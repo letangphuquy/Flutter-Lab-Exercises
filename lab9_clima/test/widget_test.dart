fc27422f293e209e531cf9bce57f400d552101dc
Le Tang Phu Quy <letangphuquy4@gmail.com>
2025-04-24 15:46:03 +0700

    Lab 9: Update flutter sdk version and pubspec to include assets

--- lab9_clima/test/widget_test.dart
+++ lab9_clima/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lab9_clima/main.dart';
+import 'package:clima/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
