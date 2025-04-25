95b1037f6001eb04b937714ef7963a426380f99b
Le Tang Phu Quy <letangphuquy4@gmail.com>
2025-04-25 09:00:39 +0700

    Lab 9: Rounding bug fix last-minute AC

--- lab9_clima/lib/screens/location_screen.dart
+++ lab9_clima/lib/screens/location_screen.dart
@@ -34,8 +34,7 @@ class _LocationScreenState extends State<LocationScreen> {
         cityName = '';
         return;
       }
-      double temp = weatherData['main']['temp'];
-      temperature = temp.toInt();
+      temperature = weatherData['main']['temp'].round();
       var condition = weatherData['weather'][0]['id'];
       weatherIcon = weatherModel.getWeatherIcon(condition);
       weatherMessage = weatherModel.getMessage(temperature!);
